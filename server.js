c602a0fd3a2e708a36761bacdbd2c69bbca673ec
Danny Pham <dpham760@gmail.com>
2020-12-02 23:59:47 -0800

    Updated server to receive new code updates and broadcast updates to all other users.

--- server.js
+++ server.js
@@ -23,11 +23,17 @@ http.listen(port, () => {
 const io = require('socket.io')(http);
 io.on('connection', (socket) => {
     console.log('User connected.');
+    socket.emit('connected', {});
 
     socket.on('message', (message) => {
         console.log('User sent a message: ' + message);
         
         io.emit('message', message);
+    });
+
+    socket.on('code_change', (message) => {
+        // Send code changes to all other users.
+        socket.broadcast.emit('code_change', message);
     });
 
     socket.on('disconnect', () => {
