4795ccf87d0a9d90d10fbf028c3cc6c9a02e5587
Danny Pham <dpham760@gmail.com>
2020-12-09 23:53:04 -0800

    Removed socketio from flask.

--- code_executor/app.py
+++ code_executor/app.py
@@ -1,12 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_socketio import SocketIO, emit
 import executor
 
 app = Flask(__name__)
 CORS(app)
-
-socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def hello():
@@ -29,22 +26,5 @@ def execute():
         'err_output': err_output.decode('utf-8') if err_output is not None else ''
     })
 
-@socketio.on('connect')
-def user_connected():
-    print('User connected.')
-    emit('connected', {'connected': True})
-    return
-
-@socketio.on('disconnect')
-def user_disconnected():
-    print('User disconnected.')
-    return
-
-@socketio.on('code_change')
-def handle_code_change(data):
-    print('User is changing code...')
-    emit('code_change', {'code': data['code']}, broadcast=True, include_self=False)
-    return
-
 if __name__ == '__main__':
-    socketio.run(app, port=8282)
+    app.run(port=8282)
